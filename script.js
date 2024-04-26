0c205e8b1a396391467d8bf8fbb8f4ffa4b1fec8
doost44 <167952067+doost44@users.noreply.github.com>
2024-04-26 13:12:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function changeMessage() {
     const welcomeMessage = document.getElementById('welcome-message');
-    welcomeMessage.textContent = 'Thanks for clicking!';
+    welcomeMessage.textContent = 'Thanks for visiting my portfolio!';
 }
